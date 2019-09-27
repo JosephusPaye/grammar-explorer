8a5d21fda88059f8e449f7179d5b4deafc4a9738
Josephus Paye II <j.paye96@gmail.com>
2019-09-27 13:59:22 +1000

    Add ability to change root non-terminal in explorer

--- src/components/GrammarExplorer.vue
+++ src/components/GrammarExplorer.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="grammar-explorer">
-    <template v-if="start">
-      <div class="bg-gray-700 text-white px-1 font-mono text-sm cursor-default table">
-        {{ start.value }}
-      </div>
+    <template v-if="selected">
+      <button
+        class="relative table bg-gray-700 text-white px-1 font-mono text-sm cursor-pointer focus:outline-none"
+        @click="dropdownOpen = !dropdownOpen"
+      >
+        <div class="flex pr-1">
+          <span class="mr-1">{{ selected.value }}</span>
+          <span v-if="dropdownOpen">▲</span>
+          <span v-else>▼</span>
+        </div>
 
-      <ui-non-terminal :non-terminal="start"></ui-non-terminal>
+        <ui-dropdown-menu
+          class="bg-gray-700 text-left"
+
+          :open="dropdownOpen"
+          :options="nonTerminals"
+          :selected.sync="selected"
+        ></ui-dropdown-menu>
+      </button>
+
+      <ui-non-terminal :non-terminal="selected"></ui-non-terminal>
     </template>
 
     <div class="p-8 text-gray-600 text-lg text-center" v-else>
@@ -16,25 +31,36 @@
 
 <script>
 import UiNonTerminal from './UiNonTerminal.vue'
+import UiDropdownMenu from './UiDropdownMenu.vue'
 
 export default {
   name: 'GrammarExplorer',
 
   components: {
     UiNonTerminal,
+    UiDropdownMenu,
   },
 
   props: {
     grammar: Object,
   },
 
+  data() {
+    return {
+      dropdownOpen: false,
+      selected: Object.values(this.grammar)[0],
+    };
+  },
+
   computed: {
     nonTerminals() {
       return Object.values(this.grammar)
     },
+  },
 
-    start() {
-      return this.nonTerminals[0]
+  watch: {
+    grammar() {
+      this.selected = Object.values(this.grammar)[0]
     },
   },
 }
---
+++ src/components/UiDropdownMenu.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="absolute left-0 z-50" v-show="open">
+    <input
+      type="text"
+      placeholder="Filter..."
+      class="leading-loose bg-gray-800 px-2 text-white border-b border-gray-700"
+      ref="inputEl"
+
+      @click.stop
+
+      v-model="filter"
+    >
+    <div class="overflow-y-scroll" :style="{ 'max-height': `calc(100vh - ${screenOffset}px)` }">
+      <div
+        class="leading-relaxed px-2 cursor-pointer hover:bg-gray-900"
+        :class="{
+          'is-selected bg-gray-800': option.value === selected.value,
+        }"
+
+        v-for="option in filteredOptions"
+
+        @click="select(option)"
+      >{{ option.value }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UiDropdownMenu',
+
+  props: {
+    options: Array,
+    selected: Object,
+    open: Boolean,
+    screenOffset: {
+      type: Number,
+      default: 96,
+    },
+  },
+
+  data() {
+    return {
+      filter: '',
+    }
+  },
+
+  computed: {
+    filteredOptions() {
+      return this.options.filter(option => {
+        return option.value.toLowerCase().includes(this.filter.toLowerCase())
+      })
+    },
+  },
+
+  watch: {
+    open(opened) {
+      if (opened) {
+        this.$nextTick(() => {
+          const selected = this.$el.querySelector('.is-selected')
+          selected && selected.scrollIntoView(false)
+
+          this.$refs.inputEl.focus()
+        })
+      } else {
+        this.filter = ''
+      }
+    },
+  },
+
+  methods: {
+    select(option) {
+      this.$emit('update:selected', option)
+    },
+  },
+}
+</script>
