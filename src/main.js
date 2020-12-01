b67dd3c36c5df3cb05bd879fcebe1cb843f04a43
Josephus Paye II <j.paye96@gmail.com>
2020-12-01 20:23:47 +1100

    Customise favicon

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+import * as emojicon from 'emojicon';
 import Vue from 'vue';
+
 import App from './App.vue';
 
 Vue.config.productionTip = false;
+emojicon.set('🎡');
 
 new Vue({
   render: (h) => h(App),
